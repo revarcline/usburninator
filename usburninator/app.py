6332127970d600e6752a2a517c3a1fbdaea8f581
Alex Cline <rev.a.r.cline@gmail.com>
2023-08-19 00:01:23 -0500

    first placeholder

--- usburninator/app.py
+++ usburninator/app.py
@@ -1,2 +1,18 @@
+from textual.app import App, ComposeResult
+from textual.widgets import Placeholder
+
+
+class USBurninator(App):
+    """Main wrapper"""
+
+    def compose(self) -> ComposeResult:
+        yield Placeholder("Hello", variant="text", id="hello")
+
+
 def start():
-    print("yooo")
+    """main function"""
+    USBurninator().run()
+
+
+if __name__ == "__main__":
+    start()
